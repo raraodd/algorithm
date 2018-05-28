45eb1ad705f510d9d34033bca0a6a9f287ee9c64
Wendy Paramandhita <wendy.p@samsung.com>
2018-05-28 18:57:48 +0700

    arithmetic expressions

---
+++ src/hackerrank/arithmeticexpressions/ArithmeticExpressions.java
@@ -0,0 +1,98 @@
+package hackerrank.arithmeticexpressions;
+
+import java.io.*;
+import java.util.*;
+
+public class ArithmeticExpressions {
+
+    private static final int MULTIPLICATION = 1;
+    private static final int ADDITION = 2;
+    private static final int SUBTRACTION = 3;
+    static String finalExpression = "";
+
+    static int doArithmetic(int[] arr, int pos, int expression, int result) {
+        int res;
+        if(pos == arr.length) return result;
+
+        switch (expression) {
+            case MULTIPLICATION:
+                result *= arr[pos];
+                break;
+            case ADDITION:
+                result += arr[pos];
+                break;
+            case SUBTRACTION:
+                result -= arr[pos];
+                break;
+        }
+
+        res = doArithmetic(arr, pos+1, 1, result);
+        if(res != -99999999 && res % 101 == 0) {
+            System.out.println(pos + " " + res + " " + res % 101);
+            finalExpression = String.valueOf(getExpression(expression) + arr[pos]) + finalExpression;
+            return res;
+        }
+
+        res = doArithmetic(arr, pos+1, 2, result);
+        if(res != -99999999 && res % 101 == 0) {
+            System.out.println(pos + " " + res + " " + res % 101);
+            finalExpression = getExpression(expression) + String.valueOf(arr[pos]) + finalExpression;
+            return res;
+        }
+
+        res = doArithmetic(arr, pos+1, 3, result);
+        if(res != -99999999 && res % 101 == 0) {
+            System.out.println(pos + " " + res + " " + res % 101);
+            finalExpression = getExpression(expression) + String.valueOf(arr[pos]) + finalExpression;
+            return res;
+        }
+
+        return -99999999;
+    }
+
+    static String getExpression(int expression) {
+        switch (expression) {
+            case MULTIPLICATION: return "*";
+            case ADDITION: return "+";
+            case SUBTRACTION: return "-";
+        }
+        return "";
+    }
+
+    // Complete the arithmeticExpressions function below.
+    static String arithmeticExpressions(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        doArithmetic(arr, 1, 1, arr[0]);
+        if(!finalExpression.equals("")) return finalExpression = arr[0] + finalExpression;
+        doArithmetic(arr, 1, 2, arr[0]);
+        if(!finalExpression.equals("")) return finalExpression = arr[0] + finalExpression;
+        doArithmetic(arr, 1, 3, arr[0]);
+        return finalExpression = arr[0] + finalExpression;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new java.io.FileInputStream("/Users/wendy/Documents/3. Git/algorithm/src/hackerrank/arithmeticexpressions/input00.txt"));
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        String result = arithmeticExpressions(arr);
+        System.out.println(result);
+
+        scanner.close();
+    }
+
+}
--- src/util/Tree.java
+++ src/util/Tree.java
@@ -2,10 +2,47 @@ package util;
 
 public class Tree <Key> {
 
-    public class Node {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
 
-        Node() {
+        Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
 
+    public static Node insert (Node head, int data) {
+        Node p = new Node(data);
+
+        if(head == null) {
+            head = p;
+        } else {
+            Node node = head;
+            while (node != null) {
+                if(node.data >= data) {
+                    node = node.right;
+                } else {
+                    node = node.left;
+                }
+            }
+            node = p;
+        }
+
+        return head;
+    }
+
+    public static void main(String args[])
+    {
+        Node head = null;
+
+        int T[] = new int[]{1,5,2,7,0};
+
+        int i = 0;
+        while (i < T.length) {
+            head = insert(head, T[i]);
+            i++;
         }
     }
 }
